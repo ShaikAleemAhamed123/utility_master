0bc05b3cdc9210647e8e119d98a0422c44bbc9a3
ShaikAleemAhamed <shaikaleemahamed@gmail.com>
2023-08-18 12:02:27 +0530

    front-end part complete

--- src/app/inputForm/index.js
+++ src/app/inputForm/index.js
@@ -3,14 +3,52 @@ import './index.css'
 
 function InputForm() {
 
+    const [equalSplit, setEqualSplit] = ["", () => {
+        if (document.querySelector("#equal-split").checked) {
+            const unequal_splits = document.querySelectorAll(".unequal-splits input");
+            for (const split of unequal_splits) {
+                split.checked = false;
+            }
+            document.querySelector(".unequal-splits").style.visibility = "hidden";
+        }
+        else {
+            document.querySelector(".unequal-splits").style.visibility = "visible";
+        }
+    }];
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const amount = document.querySelector("#amount").value;
+        if (amount < 0) {
+            alert("Enter Positive Amount");
+        }
+        else {
+            const inputs = document.querySelectorAll('.splits input');
+            let splits = false;
+            for (const input of inputs) {
+                if (input.checked) {
+                    splits = true;
+                    break;
+                }
+            }
+            if (!splits) {
+                alert("Select atleast one split option");
+            }
+            else {
+                // submit the form
+                const form = event.target;
+                form.reset();
+            }
+        }
+    }
+
     return <>
-        <form className="my-form">
+        <form className="my-form" onSubmit={submitHandler}>
             <div className="container">
                 <h1>Add Expense</h1>
                 <ul>
                     <li>
                         <select>
-                            <option selected disabled>---- Please Select an Option ----</option>
                             <option>Water Can</option>
                             <option>Laundry</option>
                             <option>Others</option>
@@ -24,11 +62,22 @@ function InputForm() {
                     </li>
 
                     <li>
-                        <input type="text" placeholder="Amount" required />
+                        <input type='number' id="amount" placeholder="Amount" required />
                     </li>
-                    <li>
-                        <input type="checkbox" id="terms" />
-                        <label for="terms">Equal Split</label>
+                    <li class='splits'>
+                        <input type="checkbox" id="equal-split" onClick={setEqualSplit} />
+                        <label htmlFor="equal-split">Equal Split</label>
+                    </li>
+                    <li class='splits'>
+                        <div className="unequal-splits grid grid-3">
+                            <input type="checkbox" id="aleem" />
+                            <label htmlFor="aleem">Aleem</label>
+                            <input type="checkbox" id="vamshi" />
+                            <label htmlFor="vamshi">Vamshi</label>
+                            <input type="checkbox" id="sai" />
+                            <label htmlFor="sai">Sai</label>
+                        </div>
+
                     </li>
                     <li>
                         <div className="grid grid-3">
