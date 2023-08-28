7e1837af9a7d0f187ebbebfa7864de937b215ede
Aleem Ahamed Shaik <81615867+ShaikAleemAhamed123@users.noreply.github.com>
2023-08-28 19:12:56 +0000

    reRouted /home to /

--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -42,7 +42,7 @@ function NavBar() {
  }
     return <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/home"><img src={app_logo} className=" me rounded-circle " alt="Aleem's" /></a>
+            <a className="navbar-brand" href="/"><img src={app_logo} className=" me rounded-circle " alt="Aleem's" /></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -51,7 +51,7 @@ function NavBar() {
             <div className="collapse navbar-collapse  " id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item me-4">
-                        <a className="nav-link" href="/home">Home</a>
+                        <a className="nav-link" href="/">Home</a>
                     </li>
                     <li className="nav-item me-4">
                         <a className="nav-link" href="/credits" onClick={handleCredits}>Credits</a>
