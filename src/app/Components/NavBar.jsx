9cd36bb156c9266ca3c9218dfcfd2e1580e4ca0b
Aleem Ahamed Shaik <81615867+ShaikAleemAhamed123@users.noreply.github.com>
2023-09-03 14:18:53 +0000

    added pending txns nav-link

--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -5,41 +5,55 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 function NavBar() {
-    const navigate=useNavigate();
-    const logOut=()=>{
+    const navigate = useNavigate();
+    const logOut = () => {
         Cookies.remove('user_token');
-    Cookies.remove('username');
+        Cookies.remove('username');
         return navigate("/login");
 
     }
-    const token=Cookies.get("user_token");
-    const userName=Cookies.get("username");
-    const handleCredits=async ()=>{
-           try{
-            const headers={
+    const token = Cookies.get("user_token");
+    const userName = Cookies.get("username");
+    const handleCredits = async () => {
+        try {
+            const headers = {
                 'Authorization': `Bearer ${token}`,
-                'userHandle':`${userName}`,
-              }
-              const res=await axios.get("https://utility-server.azurewebsites.net/user/credits",{headers});
-              console.log(res);
+                'userHandle': `${userName}`,
             }
-           catch(err){
-            console.log("Error, here in the handling credits: ",err);
-           }
-    }
-    const handleDebts=async ()=>{
-        try{
-         const headers={
-             'Authorization': `Bearer ${token}`,
-             'userHandle':`${userName}`,
-           }
-           const res=await axios.get("https://utility-server.azurewebsites.net/user/debts",{headers});
-           console.log(res);
-         }
-        catch(err){
-         console.log("Error, here in the handling debts: ",err);
+            const res = await axios.get("https://utility-server.azurewebsites.net/user/credits", { headers });
+            console.log(res);
         }
- }
+        catch (err) {
+            console.log("Error, here in the handling credits: ", err);
+        }
+    }
+    const handleDebts = async () => {
+        try {
+            const headers = {
+                'Authorization': `Bearer ${token}`,
+                'userHandle': `${userName}`,
+            }
+            const res = await axios.get("https://utility-server.azurewebsites.net/user/debts", { headers });
+            console.log(res);
+        }
+        catch (err) {
+            console.log("Error, here in the handling debts: ", err);
+        }
+    }
+
+    const handlePending = async () => {
+        try {
+            const headers = {
+                'Authorization': `Bearer ${token}`,
+                'userHandle': `${userName}`,
+            }
+            const res = await axios.get("https://utility-server.azurewebsites.net/user/pending", { headers });
+            console.log(res);
+        }
+        catch (err) {
+            console.log("Error, here in the handling pending: ", err);
+        }
+    }
     return <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div className="container-fluid">
             <a className="navbar-brand" href="/"><img src={app_logo} className=" me rounded-circle " alt="Aleem's" /></a>
@@ -57,7 +71,10 @@ function NavBar() {
                         <a className="nav-link" href="/credits" onClick={handleCredits}>Credits</a>
                     </li>
                     <li className="nav-item me-4">
-                        <a className="nav-link"  href="/debts" onClick={handleDebts}>Debts</a>
+                        <a className="nav-link" href="/debts" onClick={handleDebts}>Debts</a>
+                    </li>
+                    <li className="nav-item me-4">
+                        <a className="nav-link" href="/pending" onClick={handlePending}>Pending</a>
                     </li>
                     <li className="nav-item me-4">
                         <a className="nav-link" href="/about">About</a>
