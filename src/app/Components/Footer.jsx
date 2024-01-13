d47151011936a552aaad92f151a6a32643a7d7a0
ShaikAleemAhamed <shaikaleemahamed@gmail.com>
2024-01-13 12:05:48 +0530

    added credentials in the footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -5,27 +5,27 @@ function Footer() {
         <footer className="bg-dark text-center text-white footer">
             <div className="container p-4 pb-0">
                 <section className="mb-4">
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    {/* <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
                     ><i className="fab fa-facebook-f"></i>
-                    </a>
+                    </a> */}
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://twitter.com/terminus_being" role="button"
                     ><i className="fab fa-twitter"></i>
                     </a>
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    <a className="btn btn-outline-light btn-floating m-1" href="shaikaleemahamed786@gmail.com" role="button"
                     ><i className="fab fa-google"></i>
                     </a>
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/aleemahamedshaik/" role="button"
                     ><i className="fab fa-instagram"></i>
                     </a>
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/aleem-ahamed-shaik-81123a1ab/" role="button"
                     ><i className="fab fa-linkedin-in"></i>
                     </a>
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/ShaikAleemAhamed123" role="button"
                     ><i className="fab fa-github"></i>
                     </a>
                 </section>
