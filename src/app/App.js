7b468d5b67c2a8215e486bffe4bc6c6e5d8e4f52
ShaikAleemAhamed <shaikaleemahamed@gmail.com>
2024-01-13 11:48:25 +0530

    made footer common to all routes

--- src/app/App.js
+++ src/app/App.js
@@ -25,19 +25,16 @@ function App() {
                             <ProtectedRoute>
                                 <NavBar />
                                 <InputForm />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/signup" element={
                             <>
                             <SignUp />
-                            <Footer/>
                             </>
                         } />
                         <Route exact path="/login" element={
                             <>
                             <LoginForm />
-                            <Footer/>
                             </>
                              
                         } />
@@ -45,58 +42,52 @@ function App() {
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"receivedCredits"} userHandle={loggedInUser} type={"ReceivedCredits"} endPoint={"http://localhost:8080/user/received"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/paid" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"paidDebits"} userHandle={loggedInUser} type={"PaidDebits"} endPoint={"http://localhost:8080/user/paid"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/credits" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"credits"} userHandle={loggedInUser} type={"credits"} endPoint={"http://localhost:8080/user/credits"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/debts" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"debits"} userHandle={loggedInUser} type={"debits"} endPoint={"http://localhost:8080/user/debts"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/pendingCredits" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"pendingCredits"} userHandle={loggedInUser} type={"credits"} endPoint={"http://localhost:8080/user/pendingCredits"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/pendingDebits" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <Txnpage txnType={"PendingDebits"} userHandle={loggedInUser} type={"debits"} endPoint={"http://localhost:8080/user/pendingDebits"} />
-                                <Footer />
                             </ProtectedRoute>
                         } />
                         <Route exact path="/about" element={
                             <ProtectedRoute>
                                 <NavBar />
                                 <About />
-                                <Footer />
                             </ProtectedRoute>
 
                         } />
 
                     </Routes>
                 </div>
-
             </div>
         </Router>
+        <Footer/>
+         
     </>
 }
 
